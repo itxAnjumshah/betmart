0067194bbc068158aea7f2abe2cbc7128078c7a5
Anjum Shah <anjumshah402@gmail.com>
2024-12-05 15:11:11 +0500

    install browser routs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Home from "./pages/Home";
+import Details from "./pages/details"
+
 
 function App() {
   return (
     <>
+   
+<Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='details' element={<Details/>}/>
+
+</Routes>
+
+
     
     
     
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './Componets/Navbar/Navbar';
 import Footer from './Componets/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Navbar/>
     <App />
     <Footer/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
