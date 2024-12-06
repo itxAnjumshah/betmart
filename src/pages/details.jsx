aacfb64b047ef9d8a924d532a39210af213fd71e
Anjum Shah <anjumshah402@gmail.com>
2024-12-06 17:52:19 +0500

    other  conmm

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -5,18 +5,41 @@ import Lana from "../Componets/Lanadel/Lana";
 export default function Details() {
   return (
     <>
-      <div className="detail">
         <div className="detail-left  max-w-[80%] m-auto flex items-center  gap-8 ">
          <p className="w-[100%] sm:w-[100%]"> <Graph /></p>
         <p className=" hidden sm:flex"><Lana /></p> 
         </div>
-        <div className="detail-right"></div>
-      </div>
+        
+      
 
       <div className="detail-bottom">
-        <div className="donaltrump"></div>
+<div className="comment">
+<div className="left">
+  <img src="" alt="" />
+<div className="DONAl">
+  <h1>Donald Trump</h1>
+  <p>$1,613,654,200  Vol.</p>
 
-        <div className="donaltrump-comments"></div>
+</div>
+
+ 
+</div>
+
+<div className="right">
+    
+<h1>48%</h1>
+<button>Yes</button>
+<button>No</button>
+
+
+    </div>
+
+
+</div>
+
+
+
+        
       </div>
     </>
   );
