d53f06e5d714ab22dd906789dfef03e7653d25c2
Anjum Shah <anjumshah402@gmail.com>
2024-12-06 10:24:12 +0500

    editing  in tittle

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 
 export default function Details() {
   return (
-    <div>details</div>
+    <>
+     <div className="detail">
+      <div className="detail-left"></div>
+      <div className="detail-right"></div>
+      
+      
+      
+      </div> 
+
+      <div className="detail-bottom">
+<div className="donaltrump">
+
+
+</div>
+
+<div className="donaltrump-comments">
+
+
+
+
+
+
+</div>
+
+
+
+
+      </div>
+      
+      
+      
+      
+      
+      
+      </>
   )
 }
